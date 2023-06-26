2c0004488ae47b7613b40f3d1dd0ce5968f504f8
tamer <tamer_samara@gmail.com>
2023-06-26 17:39:16 +0300

    commit

--- descriptive/__init__.py
+++
@@ -1,3 +0,0 @@
-from .pyeda import *
-
-__all__ = pyeda.__all__
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 with open("README.md", "r") as f:
     long_description = f.read()
 
-VERSION = '0.0.1'
+VERSION = '0.0.3'
 DESCRIPTION = 'Data Visualization Functions'
 LONG_DESCRIPTION = 'The modules include functions that might be useful in general cases for data analysis ' \
                    'and data visualization. '
@@ -20,7 +20,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=setuptools.find_packages(),
-    install_requires=["Pandas", "Statsmodels", "Seaborn", "Matplotlib", "SciPy", "NumPy", "Scikit-learn"],
+    install_requires=["Pandas", "Statsmodels", "Seaborn", "Matplotlib","NumPy", "Scikit-learn"],
     keywords=['python', 'data+science', 'eda', 'data+preprocessing', 'data+analysis', 'machine+learning'],
     classifiers=[
         "Programming Language :: Python :: 3",
